9e868ac27fb471a93bfa1ad2a07aff4dea4324e2
Daniella Omokore <daniellaomokore@gmail.com>
2022-12-03 23:45:39 +0000

    update

--- serializingAndDeserializingExample.py
+++ serializingAndDeserializingExample.py
@@ -2,21 +2,31 @@
 
 import json
 
+### Json string:
+# has single quotation marks around whole thing
+
+### Python dicts
+# are ordered, changeable and don't allow duplicates
+
 # LOADS - deserializing
-json1 = '{"platform":"nokia", "ip":"1111"}'     # json string
+json1 = '{"platform":"nokia", "ip":1111}'     # json string
 dict1 = json.loads(json1)   # turns json string into python dictionary that can be now accessed
 
-print(type(json1))  # json string
-print(type(dict1))  # python dict
-print(dict1['ip'])  # accessing and printing the key;s value from the dict
+
+print(type(json1))  # json string type
+print(type(dict1))  # python dict type
+print(dict1)        # printing the python dict
+#print(dict1['ip'])  # accessing and printing the key;s value from the dict
 
 
 # DUMPS - serializing
-dict2 = {"tree": "daffodil", "food": "water"}    # python dict
-json2 = json.dumps(dict2, indent=4)   # turns python dictionary into json string
+dict2 = {"tree": "daffodil",
+         "food": "water"
+         }    # python dict
+json2 = json.dumps(dict2, indent=6)   # turns python dictionary into json string
 
-print(type(dict2))  # python dict
-print(type(json2))  # json string
+print(type(dict2))  # python dict type
+print(type(json2))  # json string type
 print(json2)        # printing the json string
 
 
